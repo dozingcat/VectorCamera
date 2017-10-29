3043c7ff61fcbc0791bc2afd0ef9c8d802634751
Brian Nenninger <bnenning@gmail.com>
2017-10-29 16:30:18 -0400

    Edge and solid color RS scripts, sync for Android Studio 3.0 final.

--- app/src/main/rs/edge.rs
+++ app/src/main/rs/edge.rs
@@ -11,18 +11,18 @@ rs_allocation gYuvInput;
 rs_allocation gColorMap;
 
 uchar4 RS_KERNEL computeEdgeWithColorMap(uint32_t x, uint32_t y) {
-  int edge = 0;
-  if (x > 0 && x < gWidth - 1 && y > 0 && y < gHeight - 1) {
-      edge = 8 * rsGetElementAtYuv_uchar_Y(gYuvInput, x, y) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x - 1, y - 1) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x - 1, y) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x - 1, y + 1) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x, y - 1) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x, y + 1) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x + 1, y - 1) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x + 1, y) -
-                 rsGetElementAtYuv_uchar_Y(gYuvInput, x + 1, y + 1);
-  }
-  int index = clamp(gMultiplier * edge, 0, 255);
-  return rsGetElementAt_uchar4(gColorMap, index);
+    int edge = 0;
+    if (x > 0 && x < gWidth - 1 && y > 0 && y < gHeight - 1) {
+        edge = 8 * rsGetElementAtYuv_uchar_Y(gYuvInput, x, y) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x - 1, y - 1) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x - 1, y) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x - 1, y + 1) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x, y - 1) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x, y + 1) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x + 1, y - 1) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x + 1, y) -
+                   rsGetElementAtYuv_uchar_Y(gYuvInput, x + 1, y + 1);
+    }
+    int index = clamp(gMultiplier * edge, 0, 255);
+    return rsGetElementAt_uchar4(gColorMap, index);
 }
