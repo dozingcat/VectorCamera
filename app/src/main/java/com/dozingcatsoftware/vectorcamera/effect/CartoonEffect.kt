b504e6e4a631826b44b52782d054bb0c4192579d
Brian Nenninger <bnenning@gmail.com>
2018-09-03 00:23:08 -0400

    cleanup

--- app/src/main/java/com/dozingcatsoftware/vectorcamera/effect/CartoonEffect.kt
+++ app/src/main/java/com/dozingcatsoftware/vectorcamera/effect/CartoonEffect.kt
@@ -5,15 +5,15 @@ import android.renderscript.*
 import com.dozingcatsoftware.util.ScriptC_planar_yuv_to_rgba
 import com.dozingcatsoftware.vectorcamera.*
 import com.dozingcatsoftware.util.reuseOrCreate2dAllocation
+import kotlin.math.roundToInt
 
 /**
- * Effect that preserves the "color" of each pixel as given by its U and V values, but replaces its
- * brightness (Y value) with its edge strength.
+ * Effect that produces a cartoon-like image by blurring and reducing the color space.
  */
 class CartoonEffect(val rs: RenderScript): Effect {
 
     private var outputAllocation: Allocation? = null
-    private val script = ScriptC_edge_color(rs)
+    // private val script = ScriptC_edge_color(rs)
     private var blurredAllocation: Allocation? = null
     private var blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
 
@@ -47,10 +47,11 @@ class CartoonEffect(val rs: RenderScript): Effect {
         blurScript.setInput(rgbAllocation)
         blurScript.forEach(blurredAllocation)
 
+        // Reduce to 2 bits for each color component. Possible RGB values are (0, 85, 170, 255).
         toonAllocation = reuseOrCreate2dAllocation(blurredAllocation,
                 rs, Element::RGBA_8888, cameraImage.width(), cameraImage.height())
         for (index in 0 until 256) {
-            val mapval = (index / 85) * 85
+            val mapval = (index / 85.0).roundToInt() * 85
             toonLutScript.setRed(index, mapval)
             toonLutScript.setGreen(index, mapval)
             toonLutScript.setBlue(index, mapval)
--- app/src/main/java/com/dozingcatsoftware/vectorcamera/effect/EffectRegistry.kt
+++ app/src/main/java/com/dozingcatsoftware/vectorcamera/effect/EffectRegistry.kt
@@ -51,6 +51,16 @@ class EffectRegistry {
                         )
                 ))
             },
+            // Blue
+            {rs, prefsFn, context ->
+                EdgeEffect.fromParameters(rs, mapOf(
+                        "colors" to mapOf(
+                                "type" to "fixed",
+                                "minColor" to listOf(0, 0, 0),
+                                "maxColor" to listOf(0, 0, 255)
+                        )
+                ))
+            },
             // Cyan
             {rs, prefsFn, context ->
                 EdgeEffect.fromParameters(rs, mapOf(
@@ -62,7 +72,28 @@ class EffectRegistry {
                 ))
             },
 
-            // Row 2, edges on non-black.
+            // Row 2
+            // Purple
+            {rs, prefsFn, context ->
+                EdgeEffect.fromParameters(rs, mapOf(
+                        "colors" to mapOf(
+                                "type" to "fixed",
+                                "minColor" to listOf(0, 0, 0),
+                                "maxColor" to listOf(255, 0, 255)
+                        )
+                ))
+            },
+            // Yellow
+            {rs, prefsFn, context ->
+                EdgeEffect.fromParameters(rs, mapOf(
+                        "colors" to mapOf(
+                                "type" to "fixed",
+                                "minColor" to listOf(0, 0, 0),
+                                "maxColor" to listOf(255, 255, 0)
+                        )
+                ))
+            },
+
             // Black on white.
             {rs, prefsFn, context ->
                 EdgeEffect.fromParameters(rs, mapOf(
@@ -73,13 +104,13 @@ class EffectRegistry {
                         )
                 ))
             },
-            // Blue on white.
+            // Green on white.
             {rs, prefsFn, context ->
                 EdgeEffect.fromParameters(rs, mapOf(
                         "colors" to mapOf(
                                 "type" to "fixed",
                                 "minColor" to listOf(255, 255, 255),
-                                "maxColor" to listOf(0, 0, 255)
+                                "maxColor" to listOf(0, 160, 0)
                         )
                 ))
             },
@@ -93,6 +124,18 @@ class EffectRegistry {
                         )
                 ))
             },
+            // Blue on white.
+            {rs, prefsFn, context ->
+                EdgeEffect.fromParameters(rs, mapOf(
+                        "colors" to mapOf(
+                                "type" to "fixed",
+                                "minColor" to listOf(255, 255, 255),
+                                "maxColor" to listOf(0, 0, 255)
+                        )
+                ))
+            },
+
+            // Row 3
             // Yellow background, 2d gradient colors.
             {rs, prefsFn, context ->
                 EdgeEffect.fromParameters(rs, mapOf(
@@ -124,7 +167,6 @@ class EffectRegistry {
                 ))
             },
 
-            // Row 3, animations.
             // Animated colors.
             // Blue-green edges on black.
             {rs, prefsFn, context ->
@@ -189,6 +231,8 @@ class EffectRegistry {
                         )
                 ))
             },
+
+            // Row 4
             // Rainbow, animated vertically on white background.
             {rs, prefsFn, context ->
                 EdgeEffect.fromParameters(rs, mapOf(
@@ -210,7 +254,6 @@ class EffectRegistry {
                 ))
             },
 
-            // Row 4.
             // Solid effects
             {rs, prefsFn, context ->
                 SolidColorEffect.fromParameters(rs, mapOf(
@@ -280,14 +323,16 @@ class EffectRegistry {
                         "prefId" to "pixelChars.FULL_COLOR"
                 ))
             },
-            {rs, prefsFn, context -> PermuteColorEffect.noOp(rs) },
-
-            {rs, prefsFn, context -> CartoonEffect.fromParameters(rs, mapOf()) },
             {rs, prefsFn, context ->
                 MatrixEffect.fromParameters(rs, mapOf(
                         "numColumns" to numAsciiColumns(prefsFn)
                 ))
-            }
+            },
+
+            // Row 6
+            {rs, prefsFn, context -> PermuteColorEffect.noOp(rs) },
+
+            {rs, prefsFn, context -> CartoonEffect.fromParameters(rs, mapOf()) }
     )
 
     fun defaultEffectCount() = baseEffects.size
